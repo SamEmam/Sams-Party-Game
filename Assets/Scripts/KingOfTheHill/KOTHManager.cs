752d30168c7bdad056a82b86025654f48756b2c2
SamEmam <samhoffmann111@hotmail.com>
2019-02-13 23:36:14 +0100

    Begun new mode King Of The Hill
    In Progress: KOTHManager.cs

---
+++ Assets/Scripts/KingOfTheHill/KOTHManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KOTHManager : MonoBehaviour
+{
+    private BoxCollider killFloor;
+    private int deathCount;
+    private int playersLeft;
+
+    public GameObject player1;
+    public GameObject player2;
+    public GameObject player3;
+    public GameObject player4;
+
+    private void Awake()
+    {
+        killFloor = GetComponent<BoxCollider>();
+        if (GameStats.Player1)
+        {
+            player1.SetActive(true);
+            playersLeft++;
+        }
+        if (GameStats.Player2)
+        {
+            player2.SetActive(true);
+            playersLeft++;
+        }
+        if (GameStats.Player3)
+        {
+            player3.SetActive(true);
+            playersLeft++;
+        }
+        if (GameStats.Player4)
+        {
+            player4.SetActive(true);
+            playersLeft++;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            deathCount++;
+        }
+    }
+}
+
---
+++ Assets/Scripts/KingOfTheHill/Platform.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour
+{
+    public float timeUntilShrink = 5f;
+    public float targetScale = 10f;
+    public float shrinkSpeed = 0.1f;
+    private bool shrinking = false;
+
+    private Vector3 tempSize;
+
+    void Shrink()
+    {
+        shrinking = true;
+    }
+
+    void Update()
+    {
+        timeUntilShrink -= Time.deltaTime;
+        if (timeUntilShrink <= 0)
+        {
+            Shrink();
+        }
+
+        if (shrinking)
+        {
+            tempSize = transform.localScale;
+            tempSize.x -= Time.deltaTime * shrinkSpeed;
+            tempSize.z -= Time.deltaTime * shrinkSpeed;
+            transform.localScale = tempSize;
+            
+        }
+
+        if (transform.localScale.x % 20 <= 0.05 && shrinking)
+        {
+            timeUntilShrink = 5f;
+            shrinking = false;
+        }
+
+        if (transform.localScale.x < targetScale)
+            transform.localScale = new Vector3(10, 30, 10);
+    }
+}

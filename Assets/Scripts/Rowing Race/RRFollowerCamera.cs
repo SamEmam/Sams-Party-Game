aed04858494bedf5f459a2fe1677397e34bc619a
SamEmam <samhoffmann111@hotmail.com>
2019-03-05 23:10:20 +0100

    New Rowing Race MiniGame

---
+++ Assets/Scripts/Rowing Race/RRFollowerCamera.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RRFollowerCamera : MonoBehaviour
+{
+    public Transform player;
+    Vector3 offset = new Vector3(-7.1f, 9.46f, 0);
+
+
+    // set x and z position equal to player + offset
+
+    private void Update()
+    {
+        transform.position = player.position + offset;
+    }
+}
---
+++ Assets/Scripts/Rowing Race/RRPlayer.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XboxCtrlrInput;
+
+public class RRPlayer : MonoBehaviour
+{
+    //private bool leftRow;
+    private Rigidbody rb;
+    private readonly float speed = 100f;
+    private readonly float rotation = 100f;
+    public float cooldown;
+    public ParticleSystem waterSpray;
+
+    public Animator anim;
+
+    public XboxController controller;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        if (XCI.GetAxis(XboxAxis.LeftStickX,controller) > 0.1f && cooldown > .5f)
+        {
+            rb.AddForce(transform.forward * speed * cooldown);
+            rb.AddRelativeTorque(-Vector3.up * rotation * cooldown);
+            cooldown = 0f;
+            anim.Play("LeftRow");
+        }
+
+        else if (XCI.GetAxis(XboxAxis.LeftStickX,controller) < -0.1f && cooldown > .5f)
+        {
+            rb.AddForce(transform.forward * speed * cooldown);
+            rb.AddRelativeTorque(Vector3.up * rotation * cooldown);
+            cooldown = 0f;
+            anim.Play("RightRow");
+        }
+
+        if (cooldown < 1.5f)
+        {
+            cooldown += Time.deltaTime;
+        }
+        var main = waterSpray.main;
+        var emission = waterSpray.emission;
+        main.startSpeed = rb.velocity.magnitude / 3;
+        emission.rateOverTime = rb.velocity.magnitude * 20;
+
+        transform.localEulerAngles = new Vector3(0, transform.localEulerAngles.y, 0);
+    }
+
+
+
+
+}
